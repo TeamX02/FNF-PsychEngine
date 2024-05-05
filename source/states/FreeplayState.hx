167eac0146eb2522e0e96515849966581c0fbe40
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-05-05 12:50:43 +0300

    update source/states/FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -160,9 +160,15 @@ class FreeplayState extends MusicBeatState
 		bottomBG.alpha = 0.6;
 		add(bottomBG);
 
+    #if mobile
+		var leText:String = "Tap on X to listen to the Song / Tap on C to open the Gameplay Changers Menu / Tap on Y to Reset your Score and Accuracy.";
+		bottomString = leText;
+		var size:Int = 16;
+		#else
 		var leText:String = "Press SPACE to listen to the Song / Press CTRL to open the Gameplay Changers Menu / Press RESET to Reset your Score and Accuracy.";
 		bottomString = leText;
 		var size:Int = 16;
+		#end
 		bottomText = new FlxText(bottomBG.x, bottomBG.y + 4, FlxG.width, leText, size);
 		bottomText.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, CENTER);
 		bottomText.scrollFactor.set();
@@ -174,6 +180,9 @@ class FreeplayState extends MusicBeatState
 		changeSelection();
 		updateTexts();
 		super.create();
+			#if mobile
+      addVirtualPad(LEFT_FULL, A_B_C_X_Y);
+			#end
 	}
 
 	override function closeSubState() {
