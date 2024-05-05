aaf5a257543c5feed515c596ac30b5a8125ab432
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-05-05 12:33:31 +0300

    update source/states/StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -176,6 +176,9 @@ class StoryMenuState extends MusicBeatState
 		changeDifficulty();
 
 		super.create();
+    #if mobile
+		addVirtualPad(LEFT_FULL, A_B_X_Y);
+		#end
 	}
 
 	override function closeSubState() {
@@ -234,13 +237,19 @@ class StoryMenuState extends MusicBeatState
 			else if (upP || downP)
 				changeDifficulty();
 
-			if(FlxG.keys.justPressed.CONTROL)
+			if(FlxG.keys.justPressed.CONTROL #if mobile || virtualPad.buttonX.justPressed #end)
 			{
+			  #if mobile
+			  removeVirtualPad();
+			  #end
 				persistentUpdate = false;
 				openSubState(new GameplayChangersSubstate());
 			}
-			else if(controls.RESET)
+			else if(controls.RESET #if mobile || virtualPad.buttonY.justPressed #end)
 			{
+			  #if mobile
+			  removeVirtualPad();
+			  #end
 				persistentUpdate = false;
 				openSubState(new ResetScoreSubState('', curDifficulty, '', curWeek));
 				//FlxG.sound.play(Paths.sound('scrollMenu'));
