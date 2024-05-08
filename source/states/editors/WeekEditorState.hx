441164327bd954ab27fdd1e51dd49f1dd45bcf5f
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-05-08 07:32:11 +0300

    update source/states/editors/WeekEditorState.hx

--- source/states/editors/WeekEditorState.hx
+++ source/states/editors/WeekEditorState.hx
@@ -431,7 +431,7 @@ class WeekEditorState extends MusicBeatState
 
 		if(!blockInput) {
 			ClientPrefs.toggleVolumeKeys(true);
-			if(FlxG.keys.justPressed.ESCAPE) {
+			if(FlxG.keys.justPressed.ESCAPE #if mobile || FlxG.android.justReleased.BACK #end) {
 				MusicBeatState.switchState(new MasterEditorMenu());
 				FlxG.sound.playMusic(Paths.music('freakyMenu'));
 			}
@@ -794,8 +794,8 @@ class WeekEditorFreeplayState extends MusicBeatState
 			}
 		} else {
 			ClientPrefs.toggleVolumeKeys(true);
-			if(FlxG.keys.justPressed.ESCAPE) {
-				MusicBeatState.switchState(new MasterEditorMenu());
+			if(FlxG.keys.justPressed.ESCAPE #if mobile || FlxG.android.justReleased.BACK #end) {
+				MusicBeatState.switchStaqte(new MasterEditorMenu());
 				FlxG.sound.playMusic(Paths.music('freakyMenu'));
 			}
 
