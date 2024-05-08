8a5f0932697787e55dad2538bdf66246e090e216
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-05-08 07:19:05 +0300

    update source/states/editors/WeekEditorState.hx

--- source/states/editors/WeekEditorState.hx
+++ source/states/editors/WeekEditorState.hx
@@ -523,11 +523,15 @@ class WeekEditorState extends MusicBeatState
 		var data:String = haxe.Json.stringify(weekFile, "\t");
 		if (data.length > 0)
 		{
+		  #if desktop
 			_file = new FileReference();
 			_file.addEventListener(#if desktop Event.SELECT #else Event.COMPLETE #end, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
 			_file.save(data, weekFileName + ".json");
+			#else
+			SUtil.saveContent(data, weekFileName + ".json");
+			#end
 		}
 	}
 	
