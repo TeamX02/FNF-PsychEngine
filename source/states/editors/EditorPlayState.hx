5f4f0a39d4169910fdd8ebf38f7e97a5274fba13
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-05-08 17:16:40 +0300

    update source/states/editors/EditorPlayState.hx

--- source/states/editors/EditorPlayState.hx
+++ source/states/editors/EditorPlayState.hx
@@ -133,7 +133,7 @@ class EditorPlayState extends MusicBeatSubstate
 		dataTxt.borderSize = 1.25;
 		add(dataTxt);
 
-		var tipText:FlxText = new FlxText(10, FlxG.height - 24, 0, 'Press ESC to Go Back to Chart Editor', 16);
+		var tipText:FlxText = new FlxText(10, FlxG.height - 24, 0, 'Press BACK to Go Back to Chart Editor', 16);
 		tipText.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		tipText.borderSize = 2;
 		tipText.scrollFactor.set();
@@ -150,11 +150,15 @@ class EditorPlayState extends MusicBeatSubstate
 		DiscordClient.changePresence('Playtesting on Chart Editor', PlayState.SONG.song, null, true, songLength);
 		#end
 		RecalculateRating();
+		#if mobile
+		addMobileControls();
+		mobileControls.visible = true;
+		#end
 	}
 
 	override function update(elapsed:Float)
 	{
-		if(controls.BACK || FlxG.keys.justPressed.ESCAPE)
+		if(controls.BACK || FlxG.keys.justPressed.ESCAPE #if mobile || FlxG.android.justReleased.BACK #end)
 		{
 			endSong();
 			super.update(elapsed);
